ed8f3fa0fa5067a073bdbc31a2d481f7f4ee4067
sayaTT38 <sayaka.f.0520@gmail.com>
2026-08-27 12:11:06 +0900

    read_last_idメソッドを削除しcreate_next_idメソッドを作成

--- lib/memo_app.rb
+++ lib/memo_app.rb
@@ -17,13 +17,16 @@ def read_memos
   end
 end
 
-def read_last_id
-  if File.exist?(LAST_ID_FILE)
-    JSON.parse(File.read(LAST_ID_FILE))
-  else
-    write_json(LAST_ID_FILE, 0)
-    0
-  end
+def create_next_id
+  last_id =
+    if File.exist?(LAST_ID_FILE)
+      JSON.parse(File.read(LAST_ID_FILE))
+    else
+      0
+    end
+  next_id = last_id + 1
+  write_json(LAST_ID_FILE, next_id)
+  next_id
 end
 
 def write_json(file, data)
@@ -47,11 +50,8 @@ end
 
 post '/memos' do
   memos = read_memos
-  last_id = read_last_id
-  next_id = last_id + 1
-  last_id = next_id
+  next_id = create_next_id
   memos[next_id.to_s] = { 'id' => next_id, 'title' => params['title'], 'content' => params['content'] }
-  write_json(LAST_ID_FILE, last_id)
   write_json(MEMOS_FILE, memos)
   redirect '/memos'
 end
